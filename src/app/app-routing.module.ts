64cec64effb4dbe34c743ae8658eee8e39a5ac13
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-21 19:20:54 +0530

    created the new file for routers only and imported it into app.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgModule } from '@angular/core';
+
+const appRoutes: Routes = [
+    
+
+    { path: "", component: HomeComponent },
+    {
+        path: "users", component: UsersComponent, children: [
+            { path: ":id/:name", component: UserComponent }
+        ]
+    },
+    {
+        path: "servers", component: ServersComponent,
+        children: [
+            { path: ":id", component: ServerComponent },
+            { path: ":id/edit", component: EditServerComponent },
+        ]
+
+
+    },
+    {
+        path: "**", redirectTo: "/not-found"
+    },
+
+    {
+        path: "not-found", component: PageNotFoundComponent
+    }
+
+
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
