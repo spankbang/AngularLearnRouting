9147c170d2147d0af1571adf341a786027e73c2b
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-21 19:14:32 +0530

    aadded page not found component !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,11 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const appRoutes: Routes = [
+  
   { path: "", component: HomeComponent },
   {
     path: "users", component: UsersComponent, children: [
@@ -29,6 +31,13 @@ const appRoutes: Routes = [
     
 
   },
+  {
+    path: "**", redirectTo: "/not-found"
+  },
+
+  {
+    path:"not-found", component:PageNotFoundComponent
+  }
   
 
 ]
@@ -41,7 +50,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
