9289ad43ee48402fd474c775e0606e7aa03ed2ba
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-21 17:06:08 +0530

    added code at user.component.ts at line 20 to 24 and user.component.html at line 4

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -17,6 +17,11 @@ export class UserComponent implements OnInit {
       name: this.activatedRoute.snapshot.params['name']
       
     }
+    this.activatedRoute.params
+      .subscribe((params:Params) => {
+        this.user.id = params['id']
+        this.user.name = params['name']
+    })
   }
 
 }
